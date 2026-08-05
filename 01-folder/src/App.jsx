1307dad7c63340b6334c4499d8d990ea805c23b2
Ram Ashish Gupta <ramashishgupta9935@gmail.com>
2026-08-05 13:29:56 +0530

    Initial commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = ()=>{
+  return  <>
+    <div id='dad'>
+    <h1 id='child1'>hello</h1>
+    <h2 id='child2'>hello</h2>
+  </div>
+  <div id='chacha'>
+    <h1 id='child3 '>hello</h1>
+    <h2 id='child4'>hello</h2>
+  </div>
+  </>
+  
+}
+
+export default App;
